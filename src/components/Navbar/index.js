9a06f331092f79c33b18fd17f0c6bdee62b9f52d
Mynor Perez <mynorbap@gmail.com>
2021-02-01 20:39:15 -0800

    Navbar - preliminary design and fields

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,116 @@
-import React from 'react'
+import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
+import { FaBars } from 'react-icons/fa'; //acces to react icons react-icons.github.io fa is for the first two letters of the icon
+import { 
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    NavLogoImg,
+    MobileIcon,
+    NavItem,
+    NavMenu,
+    NavLinks,
+    NavBtn,
+    NavBtnLink         
+} from "./NavbarElements"; //importing the elements from navbar elements to this file
 
-const navbar = () => {
+
+
+const toggleHome = () => {
+    scroll.scrollToTop();
+};
+
+const Navbar = ( {toggle} ) => {
     return (
-        <div>
-            <h1> Hello World </h1>
-        </div>
-    )
-}
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/" onClick={toggleHome}>
+                        <NavLogoImg src='/images/logo-2.png' alt='Ramirez Family Child Care'></NavLogoImg>
+                    </NavLogo>
+                    <MobileIcon onClick={toggle} >
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
 
-export default navbar
+                        <NavItem>
+                            <NavLinks 
+                            to='about'
+                            smooth={true}
+                            duration={500}
+                            spy={true}
+                            exact='true'
+                            offset={-80}
+                            >
+                                About
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks 
+                            to='services'
+                            smooth={true}
+                            duration={500}
+                            spy={true}
+                            exact='true'
+                            offset={-80}
+
+                            >
+                                Services
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks 
+                            to='gallery'
+                            smooth={true}
+                            duration={500}
+                            spy={true}
+                            exact='true'
+                            offset={-80}
+                            
+                            >
+                                Gallery
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks 
+                            to='contact'
+                            smooth={true}
+                            duration={500}
+                            spy={true}
+                            exact='true'
+                            offset={-80}
+                            >
+                                Contact Us 
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks 
+                            to='pricing'
+                            smooth={true}
+                            duration={500}
+                            spy={true}
+                            exact='true'
+                            offset={-80}
+                            >
+                                Pricing
+                            </NavLinks>
+                        </NavItem>
+
+
+                    </NavMenu>
+                    <NavBtn>
+                    <NavBtnLink to="signin" >
+                        Sign In
+
+                    </NavBtnLink>
+
+                    </NavBtn>
+
+                </NavbarContainer>
+            </Nav>
+            
+        </>
+    );
+};
+
+export default Navbar;
